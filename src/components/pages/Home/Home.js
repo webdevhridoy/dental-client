e5bfdfdf46112718e368a20d1a9b70e2724399d9
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-11-08 01:51:08 +0600

    home page designed doned

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import banner from '../../../assest/banner.png';
 import useTitle from '../../Hook/useTitle';
 import './Home.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+
 
 const Home = () => {
     useTitle('Home');
     return (
 
         <div>
-            <div className="container flex flex-col-reverse justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+            {/* Home section start  */}
+            <div className="container flex flex-col-reverse justify-center p-6 mx-auto sm:py-12 lg:py-16 lg:flex-row lg:justify-between">
                 <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
                     <h1 className="text-3xl font-bold sm:text-6xl customtitle">CREATING <span className=" text-blue-500">BEAUTIFUL</span> SMILES
                     </h1>
@@ -23,6 +27,91 @@ const Home = () => {
                 </div>
                 <div className='flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left'>
                     <img className='rounded-2xl' src={banner} alt="" />
+                </div>
+            </div>
+
+            {/* feature section start */}
+            <div className="container flex flex-col-reverse justify-center p-6 mx-auto sm:py-12 lg:py-16 lg:flex-row lg:justify-between">
+                <div className='flex flex-col justify-center items-center bg-blue-50 m-3 rounded-md shadow-md p-10 md:p-16'>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                    </span>
+                    <span className='my-3'>
+                        <h2 className='text-lg md:text-2xl font-bold text-gray-900'>Tell Us Your Complaint</h2>
+                        <p className=' text-lg mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                    </span>
+                </div>
+
+                <div className='flex flex-col justify-center items-center bg-blue-700 text-white m-3 rounded-md shadow-md p-10 md:p-16'>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                        </svg>
+
+                    </span>
+                    <span className='my-3'>
+                        <h2 className='text-lg md:text-2xl font-bold'>Tell Us Your Complaint</h2>
+                        <p className=' text-lg mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                    </span>
+                </div>
+
+                <div className='flex flex-col justify-center items-center bg-blue-50 m-3 rounded-md shadow-md p-10 md:p-16'>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
+                        </svg>
+                    </span>
+                    <span className='my-3'>
+                        <h2 className='text-lg md:text-2xl font-bold text-gray-900'>Tell Us Your Complaint</h2>
+                        <p className='text-lg mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                    </span>
+                </div>
+            </div>
+
+            {/* Services section started */}
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-16 lg:justify-center">
+                <div>
+                    <h1 className="text-3xl font-bold sm:text-6xl">Our <span className=" text-blue-500">Services</span>
+                    </h1>
+                    <p className="mt-6 mb-8 text-lg mx-2 md:mx-52 sm:mb-12">The Bandaid Dental is a place where a dentist performs procedures and treatments on patients. A dental clinic ordinarily provides basic services, such as checkups, fillings and root canals.
+                    </p>
+                </div>
+                <div className="max-w-xs rounded-md shadow-md m-3 mx-auto">
+                    <PhotoProvider>
+                        <PhotoView src="/https://i.ibb.co/gVvXd5H/doctor-with-patient-6-A8-ZF4-L.jpg">
+                            <img className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" src="https://i.ibb.co/gVvXd5H/doctor-with-patient-6-A8-ZF4-L.jpg" alt="" />
+                        </PhotoView>
+                    </PhotoProvider>
+                    <div className="flex flex-col justify-between space-y-8">
+
+                        <div className="mt-2 text-start mx-2">
+                            <h2 className="text-3xl font-semibold tracking-wide mt-2">Donec lectus leo</h2>
+                            <p className='text-lg mt-1'>Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget...</p>
+                            <div className='flex justify-between items-center mt-5'>
+                                <p className='text-lg'>Price: $524</p>
+                                <p className='text-lg'>Rating: 5*</p>
+                            </div>
+                        </div>
+
+                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-blue-700 text-white">Read more</button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Newsletter part started */}
+            <div className="container flex justify-center p-6 mx-auto sm:py-12 mb-20 mt-10 lg:py-16 lg:flex-row lg:justify-center items-center bg">
+                <div>
+                    <h1 className="text-5xl antialiased font-semibold leading-none text-center text-gray-100">Get Our Updates</h1>
+                    <p className="pt-2 pb-8 text-xl antialiased text-center dark:text-gray-100">Find out about events and other news</p>
+                    <form>
+                        <label className="input-group">
+                            <input type="text" placeholder="info@site.com" className="input input-bordered w-full" />
+                            <span>Email</span>
+                        </label>
+
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -4,37 +4,39 @@ import logo from '../../../assest/logo.png';
 
 const Header = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <div className='container px-10 mx-auto '>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link className='text-xl hover:text-blue-700'>Home</Link></li>
+                            <li><Link className='text-xl hover:text-blue-700'>Services</Link></li>
+                            <li><Link className='text-xl hover:text-blue-700'>Blogs</Link></li>
+                        </ul>
+                    </div>
+                    <Link to='/'><img className='w-32' src={logo} alt="" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
                         <li><Link className='text-xl hover:text-blue-700'>Home</Link></li>
                         <li><Link className='text-xl hover:text-blue-700'>Services</Link></li>
                         <li><Link className='text-xl hover:text-blue-700'>Blogs</Link></li>
                     </ul>
                 </div>
-                <Link to='/'><img className='w-32' src={logo} alt="" /></Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li><Link className='text-xl hover:text-blue-700'>Home</Link></li>
-                    <li><Link className='text-xl hover:text-blue-700'>Services</Link></li>
-                    <li><Link className='text-xl hover:text-blue-700'>Blogs</Link></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Link to='/login' className="bg-blue-700 px-3 py-2 text-xl text-white flex flex-row justify-center items-center rounded-lg hover:bg-blue-900 hover:text-white duration-500 ease-in-out">
-                    <span className='ml-2 flex justify-between items-center'>
-                        <span>Login</span>
-                        <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
+                <div className="navbar-end">
+                    <Link to='/login' className="bg-blue-700 px-3 py-2 text-xl text-white flex flex-row justify-center items-center rounded-lg hover:bg-blue-900 hover:text-white duration-500 ease-in-out">
+                        <span className='ml-2 flex justify-between items-center'>
+                            <span>Login</span>
+                            <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                            </span>
                         </span>
-                    </span>
-                </Link>
+                    </Link>
+                </div>
             </div>
         </div>
     );
